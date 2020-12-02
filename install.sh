5f3424bd1952d4483507671321b6a241cf86c789
Tunisiano187 <fabian@bowlman.org>
2020-12-02 13:51:32 +0100

    add vpn

--- install.sh
+++ install.sh
@@ -33,7 +33,36 @@ apt-get -y -qq update
 
 echo -e "${INFO}Installing dependencies${NC}"
 
-apt-get -y -qq install udhcpd
+apt-get -y -qq install udhcpd pptp-linux
+
+echo -e "${INFO}#######################${NC}"
+echo -e "${INFO}# Configuring the VPN #${NC}"
+echo -e "${INFO}#######################${NC}"
+
+read -r -p "What is the name of the vpn ?" remotevpnname
+read -r -p "What is the server IP or URL ? " remotevpn
+read -r -p "What is your VPN username ? " remotevpnuser
+read -r -sp "What is your VPN password ? " remotevpnpassword
+read -r -p "What is the remote network's address (ex:192.168.1.0/24)" remotevpnnet
+
+echo -e "${INFO}Setting up the VPN informations${NC}"
+echo "${remotevpnuser} ${remotevpnname} ${remotevpnpassword} *" >> /etc/ppp/chap-secrets
+echo "pty \"pptp ${remotevpn} --nolaunchpppd\"" > /etc/ppp/peers/${remotename}
+echo "name ${remotevpnuser}" >> /etc/ppp/peers/${remotename}
+echo "remotename ${remotevpnname}" >> /etc/ppp/peers/${remotename}
+echo "require-mppe-128" >> /etc/ppp/peers/${remotename}
+echo "file /etc/ppp/options.pptp" >> /etc/ppp/peers/${remotename}
+echo "ipparam ${remotevpnname}" >> /etc/ppp/peers/${remotename}
+cp ./config-files/99vpnroute /etc/ppp/ip-up.d/99vpnroute
+sed -i "s/workvpn/${remotevpnname}/g" /etc/ppp/ip-up.d/99vpnroute
+sed -i "s~192.168.1.0/24~${remotevpnnet}~g" /etc/ppp/ip-up.d/99vpnroute
+chmod +x /etc/ppp/ip-up.d/99vpnroute
+echo "#!/bin/sh" > /etc/network/if-up.d/ppp
+echo "# This file was installed with the RPI-VPN-Gateway script" >> /etc/network/if-up.d/ppp
+echo "# For more info see https://github.com/unixabg/RPI-VPN-Gateway" > /etc/network/if-up.d/ppp
+echo "" > /etc/network/if-up.d/ppp
+echo "pon ${remotevpnname}" > /etc/network/if-up.d/ppp
+
 
 echo -e "${INFO}###########################################${NC}"
 echo -e "${INFO}# copying configs to relevant directories #${NC}"
@@ -66,11 +95,12 @@ echo -e "${INFO}Searching for the network allowing us to go to the internet.${NC
 echo -e "${INFO}------------------------------------------------------------${NC}"
 _LAN=route -n | grep "UG " | tail -n1 | sed 's/[[:space:]]\{1,\}/ /g' | cut -d ' ' -f2 | cut -d '.' -f3
 #_INTERFACE = 
-echo -e "${INFO}##################################################${NC}"
+echo -e "${INFO}#########################################${NC}"
 echo -e "${INFO}# Updating local network to 192.168.${_ROUTER}.1 #${NC}"
-echo -e "${INFO}##################################################${NC}"
+echo -e "${INFO}#########################################${NC}"
 
-sed -i "s/ROUTER/${_ROUTER}/g" -e "s/DNS/${_DNS}/" /etc/udhcpd.conf
+sed -i "s/ROUTER/${_ROUTER}/g" /etc/udhcpd.conf
+#sed -i "s/DNS/${_DNS}/g" /etc/udhcpd.conf
 # Copy in the config file to enable udhcpd
 cp ./config-files/udhcpd /etc/default
 # Copy in the systemd udhcpd.service file
