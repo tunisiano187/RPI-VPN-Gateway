4e6238d4e68332212ddfae79a56c3f06e1359ca7
Tunisiano18 <tunisiano187@users.noreply.github.com>
2020-12-03 19:22:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ apt-get -y -qq update
 
 echo -e "${INFO}Installing dependencies${NC}"
 
-apt-get -y -qq install udhcpd pptp-linux
+apt-get -y -qq install pptp-linux
 
 echo -e "${INFO}#######################${NC}"
 echo -e "${INFO}# Configuring the VPN #${NC}"
@@ -68,70 +68,7 @@ echo "" >> /etc/network/if-up.d/ppp
 echo "pon ${remotevpnname}" >> /etc/network/if-up.d/ppp
 chmod a+x /etc/network/if-up.d/ppp
 
-echo '* *   * * *   root  if ! [[ $(ifconfig | grep ppp) ]]; then pon ${remotevpnname}; fi" > /etc/crond.d/pppforce
-
-echo -e "${INFO}###########################################${NC}"
-echo -e "${INFO}# copying configs to relevant directories #${NC}"
-echo -e "${INFO}###########################################${NC}"
-
-echo -e "${INFO}Configuring DHCP${NC}"
-echo -e "${INFO}----------------${NC}"
-read -r -p "Do you want to use remote DNS servers? [y/N] " remotednsresponse
-
-if [[ $remotednsresponse =~ ^([nN])$ ]]
-then
-	read -r -p "Do you wish to be protected by Cloudflare's kids DNS servers? [y/N] " unblockusdnsresponse
-	if [[ $unblockusdnsresponse =~ ^([yY][eE][sS]|[yY])$ ]]
-	then
-		cp ./config-files/udhcpd_cloudflare_kids.conf /etc/udhcpd.conf
-	else
-		read -r -p "Do you wish to be protected by Cloudflare's malware protection DNS servers? [y/N] " opendnsresponse
-		if [[ $opendnsresponse =~ ^([yY][eE][sS]|[yY])$ ]]
-		then
-			cp ./config-files/udhcpd_cloudflare_malware.conf /etc/udhcpd.conf
-		else
-			echo -e "${INFO}No other DNS servers available to choose from. Reverting to Cloudflare's default DNS.${NC}"
-			echo -e "${INFO}-------------------------------------------------------------------------------------${NC}"
-			cp ./config-files/udhcpd_cloudflare.conf /etc/udhcpd.conf
-		fi
-	fi
-fi
-
-echo -e "${INFO}Searching for the network allowing us to go to the internet.${NC}"
-echo -e "${INFO}------------------------------------------------------------${NC}"
-_LAN=$(route -n | grep "UG " | tail -n1 | sed 's/[[:space:]]\{1,\}/ /g' | cut -d ' ' -f2 | cut -d '.' -f3)
-#_INTERFACE = 
-echo -e "${INFO}#########################################${NC}"
-echo -e "${INFO}# Updating local network to 192.168.${_LAN}.1 #${NC}"
-echo -e "${INFO}#########################################${NC}"
-_ROUTER="$192.168.{_LAN}.1"
-sed -i "s/ROUTER/${_ROUTER}/g" /etc/udhcpd.conf
-#sed -i "s/DNS/${_DNS}/g" /etc/udhcpd.conf
-# Copy in the config file to enable udhcpd
-cp ./config-files/udhcpd /etc/default
-# Copy in the systemd udhcpd.service file
-cp ./config-files/udhcpd.service /lib/systemd/system/
-# Tell systemd to enable the udhcpd.service
-systemctl enable udhcpd.service
-
-echo -e "${INFO}Configuring interfaces${NC}"
-if [ -n "${_ROUTER}" ]; then
-	sed -e "s/ROUTER/${_ROUTER}/g" ./config-files/interfaces-template > /etc/network/interfaces
-else
-	cp ./config-files/interfaces /etc/network
-fi
-
-echo -e "${INFO}Configuring NAT${NC}"
-cp ./config-files/sysctl.conf /etc
-
-echo -e "${INFO}Configuring iptables${NC}"
-cp ./config-files/iptables.ipv4.nat /etc
-
-touch /var/lib/misc/udhcpd.leases
-
-echo -e "${INFO}Initialising DHCP server${NC}"
-#service udhcpd start
-#update-rc.d udhcpd enable
+echo '* *   * * *   root  if ! [[ $(ifconfig | grep ppp) ]]; then pon ${remotevpnname}; fi' > /etc/crond.d/pppforce
 
 echo -e "${INFO}================================================================${NC}"
 echo -e "${INFO}=================== Configuration complete! ====================${NC}"
